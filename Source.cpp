f06fc68af62c5341441e139d56cf6e2c131d36d6
Fatih Melih Özgün <34399010+fedqx@users.noreply.github.com>
2018-08-12 15:52:25 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <conio.h>
+#define print(x) std::cout << x << std::endl
+#define input(x) std::cin >> x
+#define pause system("pause")
+#define clearscreen system("cls")
+using namespace std;
+short cityid;
+bool quit;
+template < typename c>
+void certificate(c cid)
+{
+	
+
+	short ds;
+	vector<string> certificates;
+	std::string fname = std::to_string(cid) + ".cert";
+
+	ifstream load;
+	load.open(fname.c_str());
+
+	load >> ds;
+	string d;
+	for (int i = 0; i <= (ds - 1); i++)
+	{
+
+		load >> d;
+
+		certificates.push_back(d);
+
+	}
+
+	load.close();
+	
+	if (certificates.size() > 0)
+	{
+		print("certificates");
+		for (int i = 0; i < certificates.size(); i++)
+		{
+			print(certificates[i]);
+		}
+	}
+	else
+	{
+		print("These disease information are NOT Certificated. The correctness of information is UNKNOWN");
+	}
+	
+	pause;
+	
+	
+}
+template <typename v>
+void showdisease(v cid)
+{
+	clearscreen;
+	short bc;
+	bool breakloop = false;
+	short ds;
+	vector<string> diseases;
+	std::string fname = std::to_string(cid) + ".cityreport";
+
+	ifstream load;
+	load.open(fname.c_str());
+
+	load >> ds;
+	string d;
+	for (int i = 0; i <= (ds - 1); i++)
+	{
+
+		load >> d;
+
+		diseases.push_back(d);
+
+	}
+
+
+	print("Biggest diseases in current city");
+	for (int i = 0; i < diseases.size(); i++)
+	{
+		print(diseases[i]);
+	}
+	load.close();
+	
+	print("would you like to view certificates ? y/n");
+	do
+	{
+		switch (_getch())
+		{
+		case 'y':
+		{
+			certificate(cityid);
+			breakloop = true;
+		} break;
+		case 'n':
+		{
+			breakloop = true;
+			/// return to main menu
+		} break;
+		} // switch
+		
+	} while (!breakloop);
+	if (_kbhit())
+	{
+
+	
+	}
+	
+
+}
+
+bool login()
+{
+	short a;
+	short b;
+	
+	lstart:
+	print("-----Welcome to DTS(Disease Tracking System). Please login with the password your local hospital gave you------");
+	input(a);
+
+	ifstream yukle;
+	yukle.open("pass.userpass");
+
+	yukle >> b;
+	yukle.close();
+	if (b == a)
+	{
+		print("Enter your city id:");
+		input(cityid);
+
+		return true;
+
+	}
+	else if (b != a)
+	{
+		
+		print("incorrect password !");
+		pause;
+		system("cls");
+		goto lstart;
+
+	}
+	pause;
+
+
+}
+void mainmenu()
+{
+	
+	print("Disease Tracking System v1.0");
+	print("Press 'S' Show diseases in current city");
+	print("Press 'Q' to exit the program");
+	print("Press 'R' to re-enter the city id");
+	/// would you like to use number or character mode ?
+
+		switch (_getch())
+		{
+		case 's':
+		{
+
+			showdisease(cityid);
+
+		} break;
+		case 'q':
+		{
+			quit = true;
+		} break;
+		case 'r':
+		{
+			print("Enter your city id:");
+			input(cityid);
+		} break;
+
+		} // switch
+	
+	
+}
+
+int main()
+{
+	if (login() == true)
+	{
+		while (!quit)
+		{
+			clearscreen;
+			mainmenu();
+		}
+	
+
+	}
+
+
+
+
+}
+
